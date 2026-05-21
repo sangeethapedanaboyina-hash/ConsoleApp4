9f30640eaa9da81ce4978b77d70f8015717a2622
user <user@DESKTOP-DJA71PL>
2026-05-21 11:13:44 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class newclass
+    {
+        int a;
+        int b;
+        public newclass(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public void add()
+        {
+            int c = a + b;
+            Console.WriteLine("sum is: "+c);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            newclass obj = new newclass(10, 20);
+            obj.add();
+            Console.ReadLine();
+        }
+    }
+}
